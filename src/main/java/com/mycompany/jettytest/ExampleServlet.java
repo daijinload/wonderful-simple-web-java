2a7df332714aab5bc2ca29d13d0f4b78812aefce
daijinload <daijinload@gmail.com>
2019-01-02 17:11:16 +0900

    Feature/java11 (#1)
    java11対応

--- src/main/java/com/mycompany/jettytest/ExampleServlet.java
+++ src/main/java/com/mycompany/jettytest/ExampleServlet.java
@@ -13,14 +13,10 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.eclipse.jetty.http.HttpStatus;
 
-/**
- *
- * @author hashimoto
- */
 public class ExampleServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+    protected void service(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException {
         resp.setStatus(HttpStatus.OK_200);
         resp.getWriter().println("EmbeddedJetty");
